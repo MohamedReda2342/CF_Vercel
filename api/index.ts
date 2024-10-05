ed4b7f9ba3086d33f1d5a1cf3f884351347391f2
Mohamed Reda <78495546+MohamedReda2342@users.noreply.github.com>
2024-10-05 19:19:27 +0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -18,12 +18,19 @@ function strip(string) {
 
 // to get the dom
 async function getDom(url) {
-    return axios.get(url)
-    .then((res) => {
-        const dom = new JSDOM(res.data);
-        return dom.window.document;
-    })
-}
+       return axios.get(url, {
+           headers: {
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'Accept-Language': 'en-US,en;q=0.5',
+               'Referer': 'https://codeforces.com'
+           }
+       })
+       .then((res) => {
+           const dom = new JSDOM(res.data);
+           return dom.window.document;
+       })
+   }
 
 // get ac solutions for each contestant
 const getAc = async(url) => {
